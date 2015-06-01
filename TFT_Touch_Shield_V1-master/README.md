6829084d09e7457d864eab88bbab3ed27f6527ce
Dustin Masters <ceo@dustinsoftware.com>
2015-05-31 22:53:52 -0700

    Initial commit.

---
+++ TFT_Touch_Shield_V1-master/README.md
@@ -0,0 +1,51 @@
+TFT Touch Shield V1.0
+---------------------------------------------------------
+
+2.8'' TFT Touch Shield is an Arduino UNO/ Mega compatible multicolored TFT display with a 4-wire resistive touch-screen. It is available in an Arduino shield compatible pinout for attachment. The TFT driver is based on ST7781R with 8bit data and 4bit control interface. 
+ 
+**Features:**
+
+
+- Arduino UNO Rev3 / Mega Shield compatible footprint.
+- TFT
+- Resolution : 240 x 320 pixels
+- Size : 2.8" (50.0 x 69.2 x 2.5 mm)
+- Colors : 262K
+- Backlight : LED
+- Driver IC: ST7781R
+- 4-Wire resistive touch screen
+ 
+**Application Ideas:**
+
+- As a general purpose Graphic display for Arduino compatible devices and any MCU.
+- Touch based inputs.
+
+### Usage:
+
+This code include an easy example which can show you how to use this shield
+
+For more information, please refer to [wiki page](http://www.seeedstudio.com/wiki/2.8%27%27_TFT_Touch_Shield_V1.0).
+
+    
+----
+
+
+This software is written by loovee [luweicong@seeedstudio.com](luweicong@seeedstudio.com "luweicong@seeedstudio.com") for seeed studio<br>
+and is licensed under [The MIT License](http://opensource.org/licenses/mit-license.php) by getting grant from the author.<br>
+
+Contributing to this software is warmly welcomed. You can do this basically by<br>
+[forking](https://help.github.com/articles/fork-a-repo), committing modifications and then [pulling requests](https://help.github.com/articles/using-pull-requests) (follow the links above<br>
+for operating guide). Adding change log and your contact into file header is encouraged.<br>
+Thanks for your contribution.
+
+Seeed Studio is an open hardware facilitation company based in Shenzhen, China. <br>
+Benefiting from local manufacture power and convenient global logistic system, <br>
+we integrate resources to serve new era of innovation. Seeed also works with <br>
+global distributors and partners to push open hardware movement.<br>
+
+
+[![Analytics](https://ga-beacon.appspot.com/UA-46589105-3/TFT_Touch_Shield_V1)](https://github.com/igrigorik/ga-beacon)
+
+
+
+
---
+++ wordclock.ino
@@ -0,0 +1,270 @@
+
+#include <stdint.h>
+#include <TFT.h>
+#include <Time.h>
+
+// Tasks
+const int wait = 10;
+const int noTasks = 1;
+typedef struct Tasks {
+   long unsigned int previous;
+   int interval;
+   void (*function)();
+} Task;
+Task tasks[noTasks];
+
+// Buffer
+boolean prevframe[16][16];
+
+// Format: { line index, start position index, length }
+
+const int w_it[3] =        { 0,  0,  2 };
+const int w_is[3] =        { 0,  3,  2 };
+const int w_half[3] =      { 7,  0,  4 }; 
+const int w_to[3] =        { 7,  14, 2 };
+const int w_past[3] =      { 8,  0,  4 };
+const int w_oclock[3] =    { 11, 10, 6 };
+const int w_in[3] =        { 12, 0,  2 };
+const int w_the[3] =       { 12, 3,  3 };
+const int w_afternoon[3] = { 12, 7,  9 };
+const int w_noon[3] =      { 12, 12, 4 }; // part of "afternoon"
+const int w_midnight[3] =  { 4,  8,  8 };
+const int w_morning[3] =   { 13, 0,  7 };
+const int w_at[3] =        { 13, 8,  2 };
+const int w_night[3] =     { 13, 11, 5 };
+const int w_evening[3] =   { 14, 0,  7 };
+const int w_and[3] =       { 14, 8,  3 };
+const int w_cold[3] =      { 14, 12, 4 };
+const int w_cool[3] =      { 15, 0,  4 };
+const int w_warm[3] =      { 15, 6,  4 };
+const int w_hot[3] =       { 15, 12, 3 };
+const int w_el[3] =        { 9,  2,  2 };
+
+const int w_minutes[20][3] = {
+  { 0,  13, 3 }, // one
+  { 1,  0,  3 }, // two
+  { 3,  0,  5 }, // three
+  { 2,  12, 4 }, // four
+  { 2,  0,  4 }, // five
+  { 5,  0,  3 }, // six
+  { 6,  0,  5 }, // seven
+  { 5,  8,  5 }, // eight
+  { 3,  6,  4 }, // nine
+  { 1,  4,  3 }, // ten
+  { 2,  5,  6 }, // eleven
+  { 6,  10, 6 }, // twelve
+  { 1,  8,  8 }, // thirteen
+  { 4,  0,  8 }, // fourteen
+  { 7,  6,  7 }, // quarter
+  { 5,  0,  7 }, // sixteen
+  { 6,  0,  9 }, // seventeen
+  { 5,  8,  8 }, // eighteen
+  { 3,  6,  8 }, // nineteen
+  { 0,  6,  6 }  // twenty
+};
+
+const int w_hours[12][3] = {
+  { 8,  5,  3 }, // one
+  { 8,  9,  3 }, // two
+  { 11, 4,  5 }, // three
+  { 9,  7,  4 }, // four
+  { 9,  12, 4 }, // five
+  { 8,  13, 3 }, // six
+  { 10, 0,  5 }, // seven
+  { 10, 6,  5 }, // eight
+  { 10, 12, 4 }, // nine
+  { 11, 0,  3 }, // ten
+  { 10, 1,  4 }, // "even"
+  { 9,  0,  6 }  // twelve
+};
+
+char const gridLine1[] PROGMEM = "ITLISOTWENTYRONE";
+char const gridLine2[] PROGMEM = "TWOETENMTHIRTEEN";
+char const gridLine3[] PROGMEM = "FIVEMELEVENIFOUR";
+char const gridLine4[] PROGMEM = "THREEPNINETEENSU";
+char const gridLine5[] PROGMEM = "FOURTEENMIDNIGHT";
+char const gridLine6[] PROGMEM = "SIXTEENDEIGHTEEN";
+char const gridLine7[] PROGMEM = "SEVENTEENOTWELVE";
+char const gridLine8[] PROGMEM = "HALFELQUARTEROTO";
+char const gridLine9[] PROGMEM = "PASTRONESTWOISIX";
+char const gridLine10[] PROGMEM = "TWELVETFOURAFIVE";
+char const gridLine11[] PROGMEM = "SEVENMEIGHTENINE";
+char const gridLine12[] PROGMEM = "TENTTHREECOCLOCK";
+char const gridLine13[] PROGMEM = "INOTHENAFTERNOON";
+char const gridLine14[] PROGMEM = "MORNINGSATENIGHT";
+char const gridLine15[] PROGMEM = "EVENINGCANDTCOLD";
+char const gridLine16[] PROGMEM = "COOLETWARMURHOTA";
+const char* const PROGMEM characterGrid[16] = {
+ gridLine1,
+ gridLine2,
+ gridLine3,
+ gridLine4,
+ gridLine5,
+ gridLine6,
+ gridLine7,
+ gridLine8,
+ gridLine9,
+ gridLine10,
+ gridLine11,
+ gridLine12,
+ gridLine13,
+ gridLine14,
+ gridLine15,
+ gridLine16
+};
+
+void setup()
+{
+  loadTasks();
+  Tft.init();  //init TFT library  
+  Tft.setDisplayDirect(RIGHT2LEFT);
+ // Tft.drawString("Testing.",80,0,1,WHITE);
+  bool emptyFrame[16][16] = {0};
+  updateDisplay(emptyFrame, emptyFrame, 1);  
+  setTime(22,46,00,31,05,2015);
+}
+
+
+void showTime() {
+  
+  // Get the time
+  
+  int h = hour() % 24;
+  int h2 = h;
+  int m = minute();
+  
+  // DEBUG
+  /*Serial.print("[DEBUG] ");
+  Serial.print(h, DEC);
+  Serial.print(':');
+  Serial.println(m, DEC);*/
+  
+  // The frame
+  boolean frame[16][16];
+  for(int r = 0; r < 16; r++) {
+    for(int c = 0; c < 16; c++) {
+      frame[r][c] = false;
+    }
+  }
+ 
+  // Show "IT IS"
+  addWordToFrame(w_it, frame);
+  addWordToFrame(w_is, frame);
+  
+  // Minutes
+  if (m == 0) {
+    
+    if (h == 0) {
+      addWordToFrame(w_midnight, frame);
+    } else if (h == 12) {
+      addWordToFrame(w_noon, frame);
+    } else {
+      addWordToFrame(w_oclock, frame);
+    }
+
+  } else {
+  
+    if (m <= 20) {
+      addWordToFrame(w_minutes[m - 1], frame);
+    } else if (m < 30) {
+      addWordToFrame(w_minutes[19], frame); // twenty
+      addWordToFrame(w_minutes[m - 21], frame);
+    } else if (m == 30) {
+      addWordToFrame(w_half, frame);
+    } else if (m < 40) {
+      addWordToFrame(w_minutes[19], frame); // twenty
+      addWordToFrame(w_minutes[60 - m - 21], frame);
+    } else {
+      addWordToFrame(w_minutes[60 - m - 1], frame);
+    }
+ 
+    if(m <= 30) {
+      addWordToFrame(w_past, frame);
+    } else {
+      addWordToFrame(w_to, frame);
+      ++h2;
+    }
+    
+  } 
+  
+  if(!(m ==0 && (h == 0 || h == 12))) {
+  
+    // Hours
+    if(h2 == 0) {
+      addWordToFrame(w_hours[11], frame);
+    } else if (h2 <= 12) {
+      addWordToFrame(w_hours[h2 - 1], frame);
+    } else {
+      addWordToFrame(w_hours[h2 - 13], frame);
+    }
+    if(h2 == 11 || h2 == 23) {
+      addWordToFrame(w_el, frame);
+    }
+  
+    // Time of day
+    if(h < 6) {
+      addWordToFrame(w_at, frame);
+      addWordToFrame(w_night, frame);
+    } else if(h < 12) {
+      addWordToFrame(w_in, frame);
+      addWordToFrame(w_the, frame);
+      addWordToFrame(w_morning, frame);
+    } else if(h < 18) {
+      addWordToFrame(w_in, frame);
+      addWordToFrame(w_the, frame);
+      addWordToFrame(w_afternoon, frame);
+    } else {
+      addWordToFrame(w_at, frame);
+      addWordToFrame(w_night, frame);
+    }
+    
+  }
+
+  // Update display
+  updateDisplay(prevframe, frame, 0);
+}
+
+void updateDisplay(boolean previousframe[16][16], boolean frame[16][16], boolean initialDraw) {
+  // todo implement for lcd 
+  for (int y = 0; y < 15; y++) {
+    for (int x = 0; x < 16; x++) {
+      if (previousframe[y][x] == frame[y][x] && !initialDraw) {
+         continue;
+      }     
+      
+      char currentLineBuffer[17];
+      strcpy_P(currentLineBuffer, (char*)pgm_read_word(&characterGrid[y]));
+      
+      previousframe[y][x] = frame[y][x];
+      Tft.drawChar(currentLineBuffer[x], 239 - x * 15, 319 - y * 21, 2, frame[y][x] ? RED : GRAY2);
+    }
+  }
+}
+
+void addWordToFrame(const int theword[3], boolean frame[16][16]){
+  for(int i = 0; i < theword[2]; ++i) {
+    frame[theword[0]][theword[1] + i] = true;
+  }
+}
+
+void loadTasks() {
+  
+  // Show time
+  tasks[0].previous = 0;
+  tasks[0].interval = 1000;
+  tasks[0].function = showTime;
+  
+}
+
+void loop() {
+  unsigned long time = millis();
+  for(int i = 0; i < noTasks; i++) {
+    Task task = tasks[i];
+    if (time - task.previous > task.interval) {
+      tasks[i].previous = time;
+      task.function();
+    }
+  }  
+  delay(wait);
+}
+
